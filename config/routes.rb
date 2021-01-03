a84719c041c727689b7803777ddbce82d34400d2
sebastian <sebastian.persson@standout.se>
2021-01-03 14:51:57 +0100

    Add route

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get  "about", to: "pages#about"
-  
-  resources :articles, only: [:show, :index]
+
+  resources :articles
 
   root 'pages#home'
 end
