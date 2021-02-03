652e5428859877949cdba98d627d7c74ab3a5d17
sebastian <sebastian.persson@standout.se>
2021-02-03 22:17:52 +0100

    Add sessions controller with create and delete

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   get  'about', to: 'pages#about'
   get 'signup', to: 'users#new'
   get 'users', to: 'users#index'
+  get 'login', to: 'sessions#new'
+
+  post 'login', to: 'sessions#create'
+
+  delete 'logout', to: 'sessions#destroy'
 end
